4e8b7da88fec8734fead03c7b2b371fbfd59ec58
Reecha Khanal <reecha.khanal@gmail.com>
2022-09-27 17:50:37 -0400

    Basic Setup to get Neighbors

--- actor.erl
+++ actor.erl
@@ -3,32 +3,84 @@
 -export[start/2].
 
 start(Id, N) ->
-    io:fwrite("I am an actor"),
-    neighbors = find2D_GridNeighbors(Id, N),
-    
-    neighbors.
+    io:fwrite("I am an actor\n"),
+    FullNetworkNeighbors = findFullNetworkNeighbors(Id, N),
+    GridNeighbors = find2DGridNeighbors(Id, N),
+    LineNeighbors = findLineGridNeighbors(Id, N),
+    ImperfectGridNeighbors = find2DIperfectGridNeighbors(Id, N),
+    GridNeighbors,
+    LineNeighbors,
+    ImperfectGridNeighbors,
+    FullNetworkNeighbors.
 
-find2D_GridNeighbors(Id, N) ->
-
-    Rows = erlang:trunc(math:sqrt(N)),
-    Cols = erlang:trunc(math:sqrt(N)),
-
-    io:fwrite(" Row: ~w  \n", [Rows]),
-
-    Neighbors = lists:append([[Id+Rows], [Id-Cols]]),
-    ModVal = Id rem Rows,
-
-    case ModVal > 1 of
-        false ->
-            if
-                ModVal == 0 ->
-                    Neighbors = lists:append(Neighbors, [Id-1]);    
-                true ->
-                    Neighbors = lists:append(Neighbors, [Id+1])
-            end;
-        true ->  
-            Neighbors = lists:append(Neighbors, [Id+1])
-    end,
-    io:fwrite("I am an actor"),
+findFullNetworkNeighbors(Id, N) ->
+    % considers everyone except itself for neighbors
+    Neighbors = lists:subtract(lists:seq(1, N), [Id]),
     Neighbors.
 
+find2DGridNeighbors(Id, N) ->
+
+    % assumption provided in the question is that the 2D grid is always a perfect square.
+    Rows = erlang:trunc(math:sqrt(N)),
+    ModVal = Id rem Rows,
+
+    if
+        ModVal == 1 ->
+            Neighbors = [Id+1];
+        ModVal == 0 ->
+            Neighbors = [Id-1];
+        true ->
+            Neighbors = lists:append([[Id-1], [Id+1]])
+    end,
+
+    if
+        Id+Rows > N ->
+            Neighbors2 = Neighbors;
+        true ->
+            Neighbors2 = lists:append([Neighbors, [Id+Rows]])
+    end,
+    if
+        Id-Rows < 1 ->
+            Neighbors3 = Neighbors2;
+        true ->
+            Neighbors3 = lists:append([Neighbors2, [Id-Rows]])
+    end,
+    Neighbors3.
+
+findLineGridNeighbors(Id, N) ->
+
+    if
+        Id > N -> 
+            Neighbors = [];
+        Id < 1 ->
+            Neighbors = [];
+        Id + 1 > N ->
+            if
+                Id - 1 < 1 ->
+                    Neighbors = [];
+                true ->
+                    Neighbors = [Id-1],
+                    io:fwrite("~w",[Id-1])
+            end;           
+        true ->
+            if
+                Id - 1 < 1 ->
+                    Neighbors = [Id+1];
+                true ->
+                    Neighbors = [Id-1, Id+1]
+            end
+    end,
+    Neighbors.
+
+find2DIperfectGridNeighbors(Id, N) ->
+    
+    ImmediateNeighbors = find2DGridNeighbors(Id, N),
+
+    NeighborsToBeIgnored = lists:append([ImmediateNeighbors, [Id]]),
+    RemainingNeighbors = lists:subtract(lists:seq(1, N), NeighborsToBeIgnored),
+
+    RandomRemaningNeighbor = lists:nth(rand:uniform(length(RemainingNeighbors)), RemainingNeighbors),
+    RandomImmediateNeighbor = lists:nth(rand:uniform(length(ImmediateNeighbors)), ImmediateNeighbors),
+
+    FinalNeighbors = lists:append([[RandomRemaningNeighbor], [RandomImmediateNeighbor]]),
+    FinalNeighbors.
