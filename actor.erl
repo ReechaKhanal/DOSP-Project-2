fef721f177606f1ba5499183040c5c71ddb4b3a4
Anjali Gupta <anjaligupta0621@gmail.com>
2022-10-07 12:58:33 -0700

    actors get created, neighbors returned, and flow of code done

--- actor.erl
+++ actor.erl
@@ -1,26 +1,88 @@
 -module(actor).
+-import(math, []).
+-export[start/0, startActors/2, startGossip/2].
 
--export[start/2].
+start() ->
 
-start(Id, N) ->
-    io:fwrite("I am an actor\n"),
-    FullNetworkNeighbors = findFullNetworkNeighbors(Id, N),
-    GridNeighbors = find2DGridNeighbors(Id, N),
-    LineNeighbors = findLineGridNeighbors(Id, N),
-    ImperfectGridNeighbors = find2DIperfectGridNeighbors(Id, N),
-    GridNeighbors,
-    LineNeighbors,
-    ImperfectGridNeighbors,
-    FullNetworkNeighbors.
+    {ok, [Nodes]} = io:fread("\nEnter the number of nodes: ", "~d\n"),
+    {ok, [Topology]} = io:fread("\nEnter Topology: ", "~s\n"),
+    {ok, [Algorithm]} = io:fread("\nEnter the Algorithm (Gossip or PushSum): ", "~s\n"),
+
+    if
+        Topology == "2D" ->
+            NumberOfNodes = getNextSquare(Nodes);
+        Topology == "imp2D" ->
+            NumberOfNodes = getNextSquare(Nodes);
+        true ->
+            NumberOfNodes = Nodes
+    end,
+
+    io:format("Number of Nodes: ~p\n", [NumberOfNodes]), % Where numNodes is the number of actors involved
+    io:format("Topology: ~p\n", [Topology]), % Topology Options: Full Network, 2D Grid, Line, Imperfect 3D Grid
+    io:format("Algorithm: ~p\n", [Algorithm]), % Algorithm Options: Gossip, Push-Sum
+
+    %Actors = createActors(NumberOfNodes).
+
+    case Algorithm of
+        "gossip" -> startGossip(NumberOfNodes, Topology);
+        "pushsum" -> io:format("This is pushsum")
+    end.
+
+    %GridNeighbors,
+    %LineNeighbors,
+    %ImperfectGridNeighbors,
+    %FullNetworkNeighbors.
+
+startGossip(NumberOfNodes, Topology) ->
+    io:format('Starting the Gossip Algorithm \n'),
+    io:format('Choosing a random actor from the given number of actors. \n'),
+
+    Actors = createActors(NumberOfNodes),
+    {Reference, ChosenActor} = lists:nth(rand:uniform(length(Actors)), Actors),
+    io:format("\nThe chosen actor is : ~p \n", [ChosenActor]),
+    Neighbors = buildTopology(Topology, Actors, NumberOfNodes, ChosenActor),
+    io:format("\nThe neighbors of the chosen node ~p for the topology ~p are ~p\n",[ChosenActor,Topology,Neighbors]).
+
+
+%%    recurseGossip(chosenActor).
+
+    % The count for the chosen actor has to be updated now.
+    % A function for that has to be written
+
+% START: Pick the first actor and tell the actor a gossip.
+% STEP: Each actor selects a random neighbor and tells it the rumor.
+% Termination: Each actor keeps track of rumors and how many times has he heard the rumor.
+
+% Gossip type algorithms can be used both for group communication and for aggregate computation.
+% Gossip Algorithm for information propagation | The Gossip algorithm involves the following:
+% 1. Starting: A participant(actor) told/sent a rumor (fact) by the main process
+% 2. Step: Each actor selects a random neighbor and tells it the rumor.
+% 3. Termination: Each actor keeps track of rumors and how many times he has heard the rumor.
+%    It stops transmitting once it has heard the rumor 10 times (10 is arbitrary, you can select other values).
+
+
+%%recurseGossip(chosenActor) ->
+%%    io:format("Recursive Gossip").
+
+
+buildTopology(Topology, Actors, NumberOfNodes, Id) ->
+    case Topology of
+        "full" -> findFullNetworkNeighbors(Id, NumberOfNodes);
+        "2D" -> find2DGridNeighbors(Id, NumberOfNodes);
+        "line" -> findLineGridNeighbors(Id, NumberOfNodes);
+        "imp2D" -> find2DIperfectGridNeighbors(Id, NumberOfNodes)
+    end.
 
 findFullNetworkNeighbors(Id, N) ->
     % considers everyone except itself for neighbors
+    io:format("\nEntering into Full\n"),
     Neighbors = lists:subtract(lists:seq(1, N), [Id]),
     Neighbors.
 
 find2DGridNeighbors(Id, N) ->
 
     % assumption provided in the question is that the 2D grid is always a perfect square.
+    io:format("\nEntering into 2D\n"),
     Rows = erlang:trunc(math:sqrt(N)),
     ModVal = Id rem Rows,
 
@@ -49,6 +111,8 @@ find2DGridNeighbors(Id, N) ->
 
 findLineGridNeighbors(Id, N) ->
 
+    io:format("\nEntering into Line\n"),
+
     if
         Id > N -> 
             Neighbors = [];
@@ -73,6 +137,8 @@ findLineGridNeighbors(Id, N) ->
     Neighbors.
 
 find2DIperfectGridNeighbors(Id, N) ->
+
+    io:format("\nEntering into Imperfect 2D\n"),
     
     ImmediateNeighbors = find2DGridNeighbors(Id, N),
 
@@ -84,3 +150,23 @@ find2DIperfectGridNeighbors(Id, N) ->
 
     FinalNeighbors = lists:append([[RandomRemaningNeighbor], [RandomImmediateNeighbor]]),
     FinalNeighbors.
+
+startActors(Id, N) ->
+    io:fwrite("I am an actor with Id : ~w\n", [Id]).
+
+
+createActors(N) ->
+
+    Actors = [  % { {Pid, Ref}, Id }
+        {spawn(actor, startActors, [Id, N]), Id }
+        || Id <- lists:seq(1, N)
+    ],
+
+    Actors.
+
+
+getNextSquare(NumberOfNodes) ->
+    SquaredNumber =  round(math:pow(math:ceil(math:sqrt(NumberOfNodes)),2)),
+    SquaredNumber.
+
+
--- input.erl
+++ input.erl
@@ -1,20 +1,30 @@
 -module(input).
 -import(string,[equal/2, lower/1]). 
--export[start/3, startGossip/1].
+-export[start/0, startGossip/1].
 
-start(numNodes, topology, algorithm) ->
+start() ->
 
-    io:format("Number of Nodes: ~p\n", [numNodes]), % Where numNodes is the number of actors involved 
-    io:format("Topology: ~p\n", [topology]), % Topology Options: Full Network, 2D Grid, Line, Imperfect 3D Grid
-    io:format("Algorithm: ~p\n", [algorithm]), % Algorithm Options: Gossip, Push-Sum
+    {ok, [NumberOfNodes]} = io:fread("\nEnter the number of nodes: ", "~d\n"),
+    {ok, [Topology]} = io:fread("\nEnter Topology: ", "~s\n"),
+    {ok, [Algorithm]} = io:fread("\nEnter the Algorithm (Gossip or PushSum): ", "~s\n"),
 
-    startGossip(numNodes).
+    io:format("Number of Nodes: ~p\n", [NumberOfNodes]), % Where numNodes is the number of actors involved
+    io:format("Topology: ~p\n", [Topology]), % Topology Options: Full Network, 2D Grid, Line, Imperfect 3D Grid
+    io:format("Algorithm: ~p\n", [Algorithm]), % Algorithm Options: Gossip, Push-Sum
 
-startGossip(N) ->
-    io:format('Starting the Gossip Algorithm'),
-    
-    Actors = createActors(N),
-    Actors.
+    if
+        Topology == "2D" or Topology == "imp2D" ->
+            NumberOfNodes = getNextSquare(NumberOfNodes)
+    end,
+    Actors = createActors(NumberOfNodes),
+    startGossip(Actors).
+
+
+startGossip(AllNodes) ->
+    io:format('Starting the Gossip Algorithm \n'),
+    io:format('Choosing a random actor from the given number of actors.'),
+
+    ChosenActor = lists:nth(rand:uniform(length(AllNodes)), AllNodes).
 
     % START: Pick the first actor and tell the actor a gossip.
     % STEP: Each actor selects a random neighbor and tells it the rumor.
@@ -26,6 +36,9 @@ startGossip(N) ->
     % 2. Step: Each actor selects a random neighbor and tells it the rumor.
     % 3. Termination: Each actor keeps track of rumors and how many times he has heard the rumor. 
     %    It stops transmitting once it has heard the rumor 10 times (10 is arbitrary, you can select other values).
+
+startPushSum(allNodes, startTime) ->
+    io:format("All nodes received").
 
 createActors(N) ->
 
